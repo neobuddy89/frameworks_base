18ff0d7b7664e46ae7af4606ec831a7eecb8d808
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-02 16:35:09 -0300

    pocket: Pocket lock improvements
    * Fix auto hide not working correctly
    * Restore systemui visibility when hidden
    
    Change-Id: I2bb57b56cdf431d32cdbc3c67f272bf982518884
    Signed-off-by: Subhajeet Muhuri <kenny3fcb@gmail.com>
    Signed-off-by: iKeramat <ikeramat80@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1619,7 +1619,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         @Override
         public void run() {
-            mDefaultDisplayPolicy.takeScreenshot(mScreenshotType, mScreenshotSource);
+            if (!mPocketLockShowing){
+                mDefaultDisplayPolicy.takeScreenshot(mScreenshotType, mScreenshotSource);
+            }
         }
     }
 
@@ -5010,12 +5012,18 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return;
         }
 
+        if (mPowerManager.isInteractive() && !isKeyguardShowingAndNotOccluded()){
+            return;
+        }
+
         if (DEBUG_INPUT) {
             Log.d(TAG, "showPocketLock, animate=" + animate);
         }
 
         mPocketLock.show(animate);
         mPocketLockShowing = true;
+
+        mPocketManager.setPocketLockVisible(true);
     }
 
     /**
@@ -5037,6 +5045,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         mPocketLock.hide(animate);
         mPocketLockShowing = false;
+
+        mPocketManager.setPocketLockVisible(false);
     }
 
     private void handleHideBootMessage() {
