3d436e7cfebb979fc312653413cf335909d2e0f8
ezio84 <brabus84@gmail.com>
2019-12-15 10:38:37 +0100

    base: Allow to set gestures back swipe deadzone [1/2]
    So it's easier to open apps left drawer or avoid unwanted back swipes
    when using gallery apps.
    
    Change-Id: I805941e7b9de44f3ffdb115ccaad9fa8c0712576
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4850,6 +4850,16 @@ public final class Settings {
          */
 
         /**
+         * Set edge gesture Y dead zone
+         * 0 - disabled - stock behavior - default
+         * 1 - smaller top area of the screen
+         * 2 - small top area of the screen
+         * 3 - half top of the screen
+         * @hide
+         */
+        public static final String EDGE_GESTURE_Y_DEAD_ZONE = "edge_gesture_y_dead_zone";
+
+        /**
          * Adaptive playback
          * Automatically pause media when the volume is muted and
          * will resume automatically when volume is restored.
