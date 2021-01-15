b6396111db19bc2cec2da0dc3c356536e6ab58a8
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-01-15 23:03:30 +0530

    SystemUI: Show keyguard affordance icons

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -787,7 +787,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
             if (mLeftIsVoiceAssist) {
                 mIconState.isVisible = mUserSetupComplete && mShowLeftAffordance;
                 if (mLeftAssistIcon == null) {
-                    mIconState.drawable = mContext.getDrawable(R.drawable.ic_mic_26dp);
+                    mIconState.drawable = mContext.getDrawable(R.drawable.ic_mic_wave);
                 } else {
                     mIconState.drawable = mLeftAssistIcon;
                 }
@@ -796,8 +796,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
             } else {
                 mIconState.isVisible = mUserSetupComplete && mShowLeftAffordance
                         && isPhoneVisible();
-                mIconState.drawable = mContext.getDrawable(
-                        com.android.internal.R.drawable.ic_phone);
+                mIconState.drawable = mContext.getDrawable(R.drawable.ic_phone_wave);
                 mIconState.contentDescription = mContext.getString(
                         R.string.accessibility_phone_button);
             }
@@ -821,7 +820,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
                     && mShowCameraAffordance
                     && mUserSetupComplete
                     && resolveCameraIntent() != null;
-            mIconState.drawable = mContext.getDrawable(R.drawable.ic_camera_alt_24dp);
+            mIconState.drawable = mContext.getDrawable(R.drawable.ic_camera_wave);
             mIconState.contentDescription =
                     mContext.getString(R.string.accessibility_camera_button);
             return mIconState;
