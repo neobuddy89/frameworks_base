5e45491d064f5f0c60822d9face65a72cf477399
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-26 01:22:54 +0530

    Revert "overlays: Remove settings icon packs"
    This reverts commit ab8e04ba60aa8619b1dd7651a28c5091128d9664.

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -32,26 +32,32 @@ LOCAL_REQUIRED_MODULES := \
 	AccentColorTangerineOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
+	IconPackCircularSettingsOverlay \
 	IconPackCircularSystemUIOverlay \
 	IconPackCircularThemePickerOverlay \
 	IconPackVictorAndroidOverlay \
     IconPackVictorLauncherOverlay \
+    IconPackVictorSettingsOverlay \
     IconPackVictorSystemUIOverlay \
     IconPackVictorThemePickerOverlay \
     IconPackSamAndroidOverlay \
     IconPackSamLauncherOverlay \
+    IconPackSamSettingsOverlay \
     IconPackSamSystemUIOverlay \
     IconPackSamThemePickerOverlay \
     IconPackKaiAndroidOverlay \
     IconPackKaiLauncherOverlay \
+    IconPackKaiSettingsOverlay \
     IconPackKaiSystemUIOverlay \
     IconPackKaiThemePickerOverlay \
 	IconPackFilledAndroidOverlay \
 	IconPackFilledLauncherOverlay \
+	IconPackFilledSettingsOverlay \
 	IconPackFilledSystemUIOverlay \
 	IconPackFilledThemePickerOverlay \
 	IconPackRoundedAndroidOverlay \
 	IconPackRoundedLauncherOverlay \
+	IconPackRoundedSettingsOverlay \
 	IconPackRoundedSystemUIOverlay \
 	IconPackRoundedThemePickerOverlay \
 	IconShapePebbleOverlay \
